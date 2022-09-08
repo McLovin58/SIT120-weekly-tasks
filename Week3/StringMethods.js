81446319318357c232fc732e2be4947d7c8b0375
McLovin58 <87600767+McLovin58@users.noreply.github.com>
2022-09-09 03:57:18 +1000

    Week 3 Task 2 complete

---
+++ Week3/StringMethods.js
@@ -0,0 +1,8 @@
+var DunderMifflin = "Michael, Jim, Dwight, Pam, Creed, Jan"
+
+
+console.log(DunderMifflin.length);                
+console.log(DunderMifflin.charAt(5));  
+console.log(DunderMifflin.lastIndexOf("i"));  
+console.log(DunderMifflin.toUpperCase());  
+console.log(DunderMifflin.toLowerCase());  
