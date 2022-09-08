a67fc878dcd89faf7e94eebcff4c9f2f269364c5
McLovin58 <87600767+McLovin58@users.noreply.github.com>
2022-09-09 03:56:23 +1000

    Week 3 task 1 finished

---
+++ Week3/NumberArrayMethods.js
@@ -0,0 +1,8 @@
+const randomnumbers = [4.76, 3, 942.93, 6, 11]
+const randomwords = ["Pineapple", "Battery", "Fridge", "Door"]
+
+console.log(randomnumbers.concat(randomwords));
+console.log(randomnumbers.pop());
+console.log(randomnumbers.length);
+console.log(randomwords.keys(2));
+console.log(randomwords.sort())
